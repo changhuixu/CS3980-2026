2038bbae36409d1a77e93603eace0672229cba94
Changhui Xu <changhuixu@hotmail.com>
2026-02-17 13:47:50 -0600

    update about router

--- Module04/main.py
+++ Module04/main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI, Response, status
+from fastapi import APIRouter, FastAPI, Response, status
+
+from tesla_models import TeslaModel
 
 
 app = FastAPI()
+
+router = APIRouter(prefix="/tesla", tags=["Tesla"])
+items_router = APIRouter(prefix="/items", tags=["Items"])
 
 
 @app.get("/")
@@ -9,17 +14,34 @@ async def welcome() -> dict:
     return {"message": "hello"}
 
 
-@app.get("/items")
+@items_router.get("/")
 async def get_items() -> list[str]:
     return ["item1", "item2"]
 
 
-@app.get("/items")  # this API wins
+@items_router.get("/")  # this API wins
 async def get_items2() -> list[str]:
     return ["item3", "item4"]
 
 
-@app.get("/items/{id}")
+@app.get("/users/me")
+async def get_me() -> str:
+    return "me"
+
+
+@app.get("/users/me")  # the previous API is overwritten
+async def get_me2() -> str:
+    return "me2"
+
+
+@app.get(
+    "/users/{id}"
+)  # if passing {id}="me", then the previous API will be matched first and return
+async def get_by_id(id) -> str:
+    return "here " + id
+
+
+@items_router.get("/{id}")
 async def get_item_by_id(id: int, response: Response) -> dict | None:
     if id == 1:
         return {"name": "item1"}
@@ -30,7 +52,7 @@ async def get_item_by_id(id: int, response: Response) -> dict | None:
         return None
 
 
-@app.post("/items")
+@items_router.post("/")
 async def add_new_item(new_item: str) -> list[str]:
     items = ["item1", "item2"]
     items.append(new_item)
@@ -38,3 +60,16 @@ async def add_new_item(new_item: str) -> list[str]:
 
 
 add_new_item("hello")
+
+
+@router.get("/{name}")
+async def get_tesla_model(name: TeslaModel) -> str:
+    if name is TeslaModel.model_x:
+        return "price: 2000"
+    if name is not TeslaModel.model_s:
+        return "Model S: $30,000"
+    return f"Model {name}, do you have budget?"
+
+
+app.include_router(router)
+app.include_router(items_router)
---
+++ Module04/tesla_models.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class TeslaModel(str, Enum):
+    model_3 = "3"
+    model_s = "S"
+    model_x = "X"
+    model_y = "Y"
